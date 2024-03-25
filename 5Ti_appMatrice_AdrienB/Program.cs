0cca4b1af60c05cb16f4b5c9befaf5931026d0c7
TheRex <83464936+Trex2007@users.noreply.github.com>
2024-03-25 20:47:24 +0100

    Add files via upload

---
+++ 5Ti_appMatrice_AdrienB/Program.cs
@@ -0,0 +1,68 @@
+﻿using _5TI_Tri2024_AdrienBrahy;
+using System;
+using System.Diagnostics;
+
+namespace _5Ti_appMatrice_AdrienB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            methodeProjet outilPro = new methodeProjet(); // isntanciation de la structure
+            string reapeatProg; // information d'utilisateur pour la reprise du programme
+            int lignesT;
+            int colonnesT;
+            int cbM = 0;
+            int mN = 0;
+            int TypeChoix = 0;
+            string mess;
+            int[,] matriceOne;
+            int[,] matriceTwo;
+            int[,] matriceRes;
+
+            do // boucle de reprise du programe
+            {
+                while (cbM < 2)
+                {
+                    outilPro.lireInt("Combien de matrices voulez-vous ?", out cbM);
+                    if (cbM < 2){
+                        Console.WriteLine("Merci de choisir 2 matrices ou plus.");
+                    }
+                }
+                
+                Console.WriteLine("Choisissez les dimentions des matrices (que 2 matrices)");
+                outilPro.askSize(ref mN, out lignesT, out colonnesT);
+                outilPro.matricesAI(lignesT, colonnesT, out matriceOne);
+
+                outilPro.askSize(ref mN, out lignesT, out colonnesT);
+                outilPro.matricesAI(lignesT, colonnesT, out matriceTwo);
+
+                while (TypeChoix == 0)
+                {
+                    outilPro.lireInt("Que voulez vous faire ? (+ = 1 | * = 2)", out TypeChoix);
+                    if (TypeChoix == 0 || TypeChoix > 2)
+                    {
+                        Console.WriteLine("Merci de vouloir entrer 1 ou 2");
+                    }
+                }
+                if (TypeChoix == 1)
+                {
+                    outilPro.matrAdd(matriceOne, matriceTwo, out matriceRes);
+                } else
+                {
+                    outilPro.matrMult(matriceOne, matriceTwo, out matriceRes);
+                }
+
+                outilPro.matrConc(matriceRes, out mess);
+
+                //demande de reprise
+                Console.WriteLine("\nvoulez-vous recommencez 'O' = oui");
+                reapeatProg = Console.ReadLine();
+                if (reapeatProg == "O")
+                {
+                    Console.WriteLine("- - - - - - - -");
+                }
+            } while (reapeatProg == "O");
+        }
+    }
+}
---
+++ 5Ti_appMatrice_AdrienB/methodeProjet.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5TI_Tri2024_AdrienBrahy
+{
+    public struct methodeProjet
+    {
+        public void lireInt(string question, out int n)
+        {
+            string nUser;
+            Console.WriteLine(question);
+            nUser = Console.ReadLine();
+            while (!int.TryParse(nUser, out n))
+            {
+                Console.WriteLine("- - - - - - - -");
+                Console.WriteLine("La réponse envoyer n'est pas valide, vueillez réaltérer la question.");
+                Console.WriteLine("- - - - - - - -");
+                Console.WriteLine(question);
+                nUser = Console.ReadLine();
+            }
+        }
+        public void askSize(ref int mN, out int lignesT, out int colonnesT)
+        {
+            mN = mN + 1;
+            lignesT = 0;
+            colonnesT = 0;
+            Console.WriteLine("\nMatrices " + mN + " :");
+            while (lignesT < 1 || colonnesT < 1)
+            {
+                lireInt("Combien de lignes désirez-vous ?", out lignesT);
+                lireInt("\nCombien de colones désirez-vous ?", out colonnesT);
+                if (lignesT < 1 || colonnesT < 1)
+                {
+                    Console.WriteLine("\nles dimentions ne sont pas bonne, merci de mettre minimum 1");
+                }
+            }
+        }
+        public void matricesAI(int lignesT, int colonnesT, out int[,] matrices)
+        {
+            matrices = new int[lignesT,colonnesT];
+            Random alea = new Random();
+            for (int i = 0; i < lignesT; i++) { 
+                for (int j = 0; j < colonnesT; j++)
+                {
+                    matrices[i, j] = alea.Next(0,10);
+                }
+            }
+        }
+        public void matrAdd(int[,]  matriceOne, int[,]  matriceTwo, out int[,]  matriceRes)
+        {
+            matriceRes = null;
+            if (matriceOne.GetLength(0) == matriceTwo.GetLength(0) && matriceOne.GetLength(1) == matriceTwo.GetLength(1))
+            {
+                for (int i = 0; i < matriceOne.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matriceOne.GetLength(1); j++)
+                    {
+                        matriceRes[i, j] = matriceOne[i, j] + matriceTwo[i, j];
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("L'addition n'est pas possible");
+            }
+            
+        }
+        public void matrMult(int[,]  matriceOne, int[,]  matriceTwo, out int[,] matriceRes)
+        {
+            matriceRes = null;
+        }
+        public void matrConc(int[,] matriceRes, out string mess)
+        {
+            mess = null;
+            for (int i = 0; i < matriceRes.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriceRes.GetLength(1); j++)
+                {
+                    mess = mess + matriceRes[i, j] + " | ";
+                }
+                mess = mess + "\n";
+            }
+        }
+    }
+}
