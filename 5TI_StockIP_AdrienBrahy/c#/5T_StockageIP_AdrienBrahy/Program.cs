4ac801c40e606f99f649ce073d44ce9f6ad89bb8
TheRex <83464936+Trex2007@users.noreply.github.com>
2024-04-23 09:06:52 +0200

    Add files via upload

---
+++ 5TI_StockIP_AdrienBrahy/c#/5T_StockageIP_AdrienBrahy/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _5T_StockageIP_AdrienBrahy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            methodeProjet outilPro = new methodeProjet(); // instanciation de la structure
+            string reapeatProg; // information d'utilisateur pour la reprise du programme
+            string[] tableNom = new string[20];
+            string mess;
+            int place = 0;
+            int[] adresseIP = new int[4];
+            int[,] MatrAdresse = new int[4, 20];
+            do // boucle de reprise du programme
+            {
+                if(place < tableNom.Length){
+                    outilPro.LireAdresseIP(adresseIP);
+                    if (outilPro.AjouteAdresseIP(place, adresseIP, MatrAdresse))
+                    {
+                        Console.WriteLine("Vôtre Adresse Ip a été ajoutée avec succès !");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Une erreur est survenue lors du traitement.");
+                    }
+                    if (outilPro.AjouteNom(place, tableNom))
+                    {
+                        Console.WriteLine("Vôtre Nom a été ajoutée avec succès !");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Une erreur est survenue lors du traitement.");
+                    }
+                    Console.WriteLine(outilPro.ConcateneAdresse(MatrAdresse, tableNom));
+                    place++;
+                }
+                
+
+                //demande de reprise
+                Console.WriteLine("\nvoulez-vous rajouter des IPs ? 'O' = oui");
+                reapeatProg = Console.ReadLine();
+                if (reapeatProg == "O")
+                {
+                    Console.WriteLine("- - - - - - - -");
+                }
+            } while (reapeatProg == "O");
+        }
+    }
+}
